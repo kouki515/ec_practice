1e0029390b753a06d8ec9853539db4545fa690f0
kouki515 <71614240+kouki515@users.noreply.github.com>
2021-03-21 00:23:01 +0900

    カート機能の実装

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 Route::name('product.')
-->group(function () {
-    Route::get('/', 'ProductController@index')->name('index');
-    Route::get('/product/{id}', 'ProductController@show')->name('show');
-});
+    ->group(function () {
+        Route::get('/', 'ProductController@index')->name('index');
+        Route::get('/product/{id}', 'ProductController@show')->name('show');
+    });
+
+Route::name('line_item.')
+    ->group(function () {
+        Route::post('/line_item/create', 'LineItemController@create')->name('create');
+    });
